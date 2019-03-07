3b0fa265179a17070afc6c1833dd617ac2f442fa
Oliver Abreu <oliverabreu@Olivers-MacBook.attlocal.net>
2019-03-06 23:13:52 -0500

    This commit should remove the hard-coded PORT number in index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 const server = require('./api/server.js');
 PORT = process.env.PORT || 4000
 
-server.listen(4000, () => {
+server.listen(PORT, () => {
   console.log(`\n*** Server Running on PORT ${PORT} ***\n`);
 });
